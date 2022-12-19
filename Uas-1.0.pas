77ec0756fbaf1d8b5dff513d4b1840b9a61a452f
Prima Hagantha <81164903+primahagantha@users.noreply.github.com>
2022-12-19 16:29:37 +0700

    merapikan code di run input dan output
    -merapikan semua teks

--- Uas-1.0.pas
+++ Uas-1.0.pas
@@ -27,7 +27,7 @@ procedure totalNilai(nilai1:real; nilai2:real; nilai3:real; nilai4:real; nilai5:
 	else
 	if Nilai >= 1.5 then
 	begin	
-	hasil:= 'F';
+	hasil	:= 'F';
 	end
 	else
 	begin
@@ -63,25 +63,25 @@ write('Masukkan berapa data mahasiswa ingin dimasukkan : '); readln(n);
 for i := 1 to n do
 	begin
 	with data[i] do 
-	begin
-	writeln('data mahasiswa ke-',i);
-		write('Masukkan nama anda 				: '); readln(nama);
-		write('Masukkan NIM anda 				: '); readln(NIM);
-		write('Masukkan Kom anda (a-b-c)			: '); readln(Kom);
-	with date do
-	begin
-		writeln('Isi tanggal-bulan-tahun saat ini');
-		write('Masukkan Tanggal saat ini (1-31) 			        :	');readln(tanggal);
-		write('Masukkan Bulan saat ini   (1-12) 			        :	');readln(bulan);
-		write('Masukkan Tahun saat ingin (0-2022)			:	');readln(tahun);
-		writeln('Isi nilai mata kuliah anda disini dengan range (1-4)');
-		write('Masukkan nilai mata kuliah Sistem-Digital 			:	 '); readln(sisdig);
-		write('Masukkan nilai mata kuliah TBO			 		:	 '); readln(tbo);
-		write('Masukkan nilai mata kuliah OAK			 		:	 '); readln(oak);
-		write('Masukkan nilai mata kuliah Matdas		 		:	 '); readln(matdas);
-		write('Masukkan nilai mata kuliah daspro		 		:	 '); readln(daspro);
-		write('Masukkan nilai mata kuliah pik			 		:	 '); readln(pik);
-		totalNilai(sisdig,tbo,oak,matdas,daspro,pik,hasil);
+		begin
+		writeln('data mahasiswa ke-',i);
+		write('Masukkan nama anda               :   '); readln(nama);
+		write('Masukkan NIM anda                :   '); readln(NIM);
+		write('Masukkan Kom anda (a-b-c)        :   '); readln(Kom);
+		with date do
+			begin
+			writeln('Isi tanggal-bulan-tahun saat ini');
+			write('Masukkan Tanggal saat ini (1-31)             :   ');readln(tanggal);
+			write('Masukkan Bulan saat ini   (1-12)             :   ');readln(bulan);
+			write('Masukkan Tahun saat ingin (0-2022)           :   ');readln(tahun);
+			writeln('Isi nilai mata kuliah anda disini dengan range (1-4)');
+			write('Masukkan nilai mata kuliah Sistem-Digital        :    '); readln(sisdig);
+			write('Masukkan nilai mata kuliah TBO                   :    '); readln(tbo);
+			write('Masukkan nilai mata kuliah OAK                   :    '); readln(oak);
+			write('Masukkan nilai mata kuliah Matdas                :    '); readln(matdas);
+			write('Masukkan nilai mata kuliah daspro                :    '); readln(daspro);
+			write('Masukkan nilai mata kuliah pik                   :    '); readln(pik);
+			totalNilai(sisdig,tbo,oak,matdas,daspro,pik,hasil);
 		end;
 	end;
 end;			
@@ -90,24 +90,24 @@ for i := 1 to n do
 		with data[i] do
 		begin
 		writeln('hasil data mahasiswa ke-',i);
-		writeln('Nama 		: ',nama);
-		writeln('Nim		        : ',nim);
-		writeln('KOM 		: ',Kom);
+		writeln('Nama       : ',nama);
+		writeln('Nim        : ',nim);
+		writeln('KOM        : ',Kom);
 		with date do 
-		begin
-		writeln('=================================');
-		writeln('Date ',tanggal,'-',bulan,'-',tahun);
-		writeln('=================================');
-		writeln('Nilai sisdig 			- ',sisdig:4:2);
-		writeln('Nilai TBO 			- ',tbo:4:2);
-		writeln('Nilai OAK 			- ',Oak:4:2);
-		writeln('Nilai matdas 			- ',matdas:4:2);
-		writeln('Nilai daspro 			- ',daspro:4:2);
-		writeln('Nilai pik 			        - ',pik:4:2);
-		writeln('=================================');
-		writeln('Nilai Ip adalah 		- ', hasil);
-		readln;
+			begin
+			writeln('=================================');
+			writeln('Date ',tanggal,'-',bulan,'-',tahun);
+			writeln('=================================');
+			writeln('Nilai sisdig       - ',sisdig:4:2);
+			writeln('Nilai TBO          - ',tbo:4:2);
+			writeln('Nilai OAK          - ',Oak:4:2);
+			writeln('Nilai matdas       - ',matdas:4:2);
+			writeln('Nilai daspro       - ',daspro:4:2);
+			writeln('Nilai pik          - ',pik:4:2);
+			writeln('=================================');
+			writeln('Nilai Ip adalah    - ', hasil);
+			readln;
+			end;
 		end;
-	end;
-end;	
+	end;	
 end.
